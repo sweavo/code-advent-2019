ad561d1c07d61a805293db9c3e2bd58145bbc41e
Steve Carter <steve.carter@etas.com>
2019-12-02 13:28:04 +0000

    move and correct doctests so we have no complaints from the runner; map the correct function over the masses of the modules.

--- 1/solution.py
+++ 1/solution.py
@@ -1,32 +1,30 @@
 #!python3
-"""
->>> import solution
->>> solution.calc_fuel(5)
-0
->>> solution.calc_fuel(12)
-2
->>> solution.calc_fuel(14)
-2
->>> solution.calc_fuel(1969)
-654
->>> solution.calc_fuel(100756)
-33583
-"""
 import sys
 
 def calc_fuel( mass ):
+    """
+    >>> solution.calc_fuel(5)
+    0
+    >>> solution.calc_fuel(12)
+    2
+    >>> solution.calc_fuel(14)
+    2
+    >>> solution.calc_fuel(1969)
+    654
+    >>> solution.calc_fuel(100756)
+    33583
+    """
     return max(0,mass // 3 - 2)
 
 def additional_fuel_generator( mass ):
     """
-    >>> import solution as S
-    >>> list(S.additional_fuel_generator(2))
+    >>> list(additional_fuel_generator(2))
     []
-    >>> list(S.additional_fuel_generator(654))
+    >>> list(additional_fuel_generator(654))
     [216, 70, 21, 5]
-    >>> list(S.additional_fuel_generator(33583))
+    >>> list(additional_fuel_generator(33583))
     [11192, 3728, 1240, 411, 135, 43, 12, 2]
-    >>> list(S.additional_fuel_generator(100756))
+    >>> list(additional_fuel_generator(100756))
     [33583, 11192, 3728, 1240, 411, 135, 43, 12, 2]
     """
     fuel = calc_fuel(mass)
@@ -49,12 +47,6 @@ if __name__ == "__main__":
     with open( 'modules.txt', 'r') as f:
         mass_strings = map( str.strip, f)
         masses = map( int, mass_strings)
-        fuels_for_modules = map( calc_fuel, masses )
-        fuel = sum(fuels_for_modules)
-
-    additional_fuel = list(additional_fuel_generator( fuel ))
-    for additional in additional_fuel:
-        print ( additional )
-
-    print (fuel + sum(additional_fuel))
+        fuels_for_modules = map( calc_total_fuel, masses )
+        print (sum(fuels_for_modules))
 
