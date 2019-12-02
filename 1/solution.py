d96986182345e34eb8951e085f52807377fbe1c9
Steve Carter <steve.carter@etas.com>
2019-12-02 09:01:34 +0000

    solved#1.1

--- 1/solution.py
+++ 1/solution.py
@@ -11,6 +11,15 @@
 33583
 >>>
 """
+import sys
+
 def calc_fuel( mass ):
     return mass // 3 - 2
+
+if __name__ == "__main__":  
+    with open( 'modules.txt', 'r') as f:
+        mass_strings = map( str.strip, f)
+        masses = map( int, mass_strings)
+        fuels = map( calc_fuel, masses )
+        print(sum(fuels))
 
