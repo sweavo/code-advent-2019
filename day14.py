26e207201f35b9169703f7f7c3eac544f4b390fb
Steve Carter <sweavo@gmail.com>
2019-12-16 18:55:13 +0000

    honing the bill of materials idea

--- day14.py
+++ day14.py
@@ -1,4 +1,5 @@
 #!python3
+import math
 import re
 
 RE_QCOMMAW=re.compile(r'\s*(\d+)\s+(\w+)\s*')
@@ -55,19 +56,47 @@ def prepare_factory( reactions ):
         result[ingredient]=(quantity,inputs)
     return result
 
+def ceildiv( needed, per_run ):
+    """ Calculate the number of runs needed if you need x and each run yields y
+    >>> ceildiv( 1, 1)
+    1
+    >>> ceildiv( 3, 2)
+    2
+    >>> ceildiv( 10, 20)
+    1
+    """
+    return math.ceil( needed / per_run )
+
 class Factory( object ):
     def __init__( self, program ):
         self._productions = prepare_factory( parse_productions( program ) )
     
-    def count( self, output_material, quantity, input_terminal ):
+    def bill_of_materials( self, output_material, quantity_needed, terminals):
         """
-        >>> Factory( "1 A => 1 B" ).count( 'B', 1, 'A' )
-        [(1, 'A')]
+        >>> Factory( "1 A => 1 B" ).bill_of_materials( 'B', 1, ['A'] )
+        ['A']
+        >>> Factory( "1 A => 1 B" ).bill_of_materials( 'B', 2, ['A'] )
+        ['A', 'A']
+        >>> Factory( "1 A, 2 B => 1 C" ).bill_of_materials( 'C', 2, ['A', 'B'] )
+        ['A', 'A', 'B', 'B', 'B', 'B']
+        >>> Factory( "1 A, 2 B => 2 C" ).bill_of_materials( 'C', 4, ['A', 'B'] )
+        ['A', 'A', 'B', 'B', 'B', 'B']
+        >>> Factory( "1 A, 2 B => 3 C" ).bill_of_materials( 'C', 4, ['A', 'B'] )
+        ['A', 'A', 'B', 'B', 'B', 'B']
+        >>> Factory( "1 A, 2 B => 4 C" ).bill_of_materials( 'C', 4, ['A', 'B'] )
+        ['A', 'B', 'B']
+        >>> Factory( "1 A, 2 B => 4 C\\n 4 A => 1 B" ).bill_of_materials( 'C', 4, ['A', 'B'] )
+        ['A', 'B', 'B']
+        >>> Factory( "1 A, 2 B => 4 C\\n 4 A => 1 B" ).bill_of_materials( 'C', 4, ['A'] )
+        ['A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A']
         """
-        yielded, recipe = self._productions[output_material]
-        if yielded>=quantity:
-            return recipe
-    
-        
-        
-     
+        run_yield, recipe = self._productions[output_material]
+        runs=ceildiv( quantity_needed, run_yield )
+        needed=[]
+        for q,i in recipe:
+            if i in terminals:
+                needed.extend( [i]*q*runs )
+            else:
+                needed.extend( self.bill_of_materials( i, q*runs, terminals ) )
+        return needed
+
