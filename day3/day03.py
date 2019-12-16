0b122da63d981d99ed5e44a241b83e927ddd8c25
Steve Carter <sweavo@gmail.com>
2019-12-16 10:13:22 +0000

    rehabilitate part2 functions having refactored part1

--- day3/day03.py
+++ day3/day03.py
@@ -1,4 +1,5 @@
 #!python3
+import functools
 
 class Turtle(object):
     def __init__(self,tape):
@@ -36,6 +37,9 @@ class Turtle(object):
             for location in eval(f"self.{op}({parm})"):
                 yield location
 
+    def distance_at_location( self, point ):
+        return self.locations.index(point)+1
+
 def manhattan_distance( point ):
     """
     >>> manhattan_distance( (10, 0) )
@@ -69,22 +73,25 @@ def day3part1():
    
     return manhattan_distance(sorted(crossings,key=manhattan_distance)[0])
 
-#def calc_cheapest_crossing( tapes ):
-#    """ 
-#    >>> calc_cheapest_crossing( [ 'R75,D30,R83,U83,L12,D49,R71,U7,L72', 'U62,R66,U55,R34,D71,R55,D58,R83' ] )
-#    610
-#    >>> calc_cheapest_crossing( [ 'R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51', 'U98,R91,D20,R16,D67,R40,U7,R15,U6,R7' ] )
-#    410
-#    """
-#    turtles = list(map(Turtle,tapes))
-#
-#    pointses = [ t.locations for t in turtles ]
-#   
-#    crossings = get_crossings( pointses ) 
-#
-#    distances = map( functools.partial( get_crossing_sum, turtles ), crossings )
-#
-#    return sorted(distances)[0]
+def get_crossing_sum( turtles, point ):
+    return sum( [ turtle.distance_at_location(point) for turtle in turtles ] )
+
+def calc_cheapest_crossing( tapes ):
+    """ 
+    >>> calc_cheapest_crossing( [ 'R75,D30,R83,U83,L12,D49,R71,U7,L72', 'U62,R66,U55,R34,D71,R55,D58,R83' ] )
+    610
+    >>> calc_cheapest_crossing( [ 'R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51', 'U98,R91,D20,R16,D67,R40,U7,R15,U6,R7' ] )
+    410
+    """
+    turtles = list(map(Turtle,tapes))
+
+    pointses = [ t.locations for t in turtles ]
+   
+    crossings = get_crossings( pointses ) 
+
+    distances = map( functools.partial( get_crossing_sum, turtles ), crossings )
+
+    return sorted(distances)[0]
 
 #def day3part2():
 #    """
