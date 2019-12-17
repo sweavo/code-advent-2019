1d8418c9b73018acbd592fccc89ade78cfb367b8
Steve Carter <sweavo@gmail.com>
2019-12-17 22:18:39 +0000

    solved day14

--- day14.py
+++ day14.py
@@ -1,7 +1,7 @@
 #!python3
 import math
 import re
-from input_day14 import PRODUCTIONS
+from input_day14 import PRODUCTIONS, EXAMPLE_13312, EXAMPLE_180697
 
 RE_QCOMMAW=re.compile(r'\s*(\d+)\s+(\w+)\s*')
 
@@ -150,12 +150,65 @@ class Factory( object ):
 
         return { k: v for k, v in self._inventory.items() if v < 0 }
 
+def ore_for_fuel( productions, quantity ):
+    factory =Factory( productions )
+    needs = factory.require(quantity,'FUEL').bill_of_materials(['ORE'])
+    return -needs['ORE']
+
 def day14part1():
     """
     >>> day14part1()
     502491
     """
-    factory =Factory( PRODUCTIONS )
-    needs = factory.require(1,'FUEL').bill_of_materials(['ORE'])
-    return -needs['ORE']
+    return ore_for_fuel( PRODUCTIONS, 1 )
+
+def binary_chop( lower, upper, func, target):
+    """ find the value between lower and upper that does not
+        exceed target """
+    #print (f'chop {lower}<{upper}...', end='')
+    if upper==lower:
+        #print('done')
+        return upper
+    else:
+        point = (upper+1+lower) // 2
+        value=func(point)
+        #print( f'compare {value} > {target}' )
+        if value > target:
+            return binary_chop( lower, point-1, func, target )
+        else:
+            return binary_chop( point, upper, func, target )
+
+TRILLION=1000000000000
+
+def fuel_from_ore(productions, quantity_of_ore, voodoo_lower=1000, voodoo_higher=1001):
+    """ we will use a binary chop to hunt for the highest value lower than 
+    TRILLION ore providing likely amounts of FUEL for input.
+    Lets start by assuming FUEL = k ORE (in fact, FUEL might be higher, because
+    ORE can result in surplus other chemicals) so let's start at a linear guess 
+    and bail if it does not require more than a TRILLION ore.
+    >>> fuel_from_ore( EXAMPLE_13312, TRILLION )
+    82892753
+    >>> fuel_from_ore( EXAMPLE_180697, TRILLION )
+    5586022
+    """
+
+    ore_for_1000_fuel=ore_for_fuel(productions, 1000)
+    fuel_for_trillion_ore_lower_bound = 1000 * quantity_of_ore // ore_for_1000_fuel
+    fuel_for_trillion_ore_upper_bound = 1001 * quantity_of_ore // ore_for_1000_fuel
+    if ore_for_fuel(productions, fuel_for_trillion_ore_lower_bound ) > quantity_of_ore:
+        raise ValueError("lower bound is too high, guess again")
+    if ore_for_fuel(productions, fuel_for_trillion_ore_upper_bound ) < quantity_of_ore:
+        raise ValueError("upper bound is too small, guess again")
+
+    return binary_chop( fuel_for_trillion_ore_lower_bound, 
+                        fuel_for_trillion_ore_upper_bound, 
+                        lambda x: ore_for_fuel(productions, x), 
+                        quantity_of_ore )
+
+def day14part2():
+    """
+    >>> day14part2()
+    2944565
+    """
+    return fuel_from_ore( PRODUCTIONS, TRILLION )
 
