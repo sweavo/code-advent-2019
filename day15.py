3197a873d94287fa628334dea2fd577dc1679f41
Steve Carter <sweavo@gmail.com>
2019-12-17 23:38:38 +0000

    instantiate a fifoputer

--- day15.py
+++ day15.py
@@ -19,29 +19,30 @@ class FIFO( object ):
 
 OPPOSITE_DIRECTION=[None, 2, 1, 4, 3]
 
-fifo=FIFO()
-intputer=day09.Intputer( tapes_day15.PROGRAM, fifo )
+class Fifoputer( day09.Intputer ):
+    def __init__(self, program ):
+        super().__init__( program, FIFO() )
 
-def request_movement( direction ):
-    fifo.append( direction )
-    return next(intputer)
+    def interact( self, input_item ):
+        self._in.append(input_item)
+        return next(self)
 
-def search_directions( request_movement, entered_by=None ):
+def search_directions( fifoputer, entered_by=None ):
     for direction in [1,2,3,4]:
         if direction==entered_by: 
             continue
-        result=request_movement( direction )
+        result=fifoputer.interact( direction )
         if result==2: # found it!
             # no need to move the bot; we will just pop the stack to measure the distance. 
             return 1 # it was 1 unit away from here
 
         elif result==1: # We moved; recurse
-            result=search_directions( request_movement, entered_by=OPPOSITE_DIRECTION[direction] )
+            result=search_directions( fifoputer, entered_by=OPPOSITE_DIRECTION[direction] )
             if result is not None: # found the goal that way
                 return result+1
         else:
             pass
-    request_movement( entered_by ) # If we are backtracking, better put the bot back where we found it.
+    fifoputer.interact( entered_by ) # If we are backtracking, better put the bot back where we found it.
     return None
    
 def day15part1():
@@ -49,9 +50,5 @@ def day15part1():
     >>> day15part1()
     212
     """
-    return search_directions(request_movement) 
-
-        
-if __name__ == "__main__":
-    print (day15part1())
+    return search_directions(Fifoputer(tapes_day15.PROGRAM) )
 
