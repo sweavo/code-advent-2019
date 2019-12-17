ab68efb458d29269b9f2d49c244a3d4727a76526
Steve Carter <sweavo@gmail.com>
2019-12-17 10:14:53 +0000

    putting the inventory into the class state

--- day14.py
+++ day14.py
@@ -70,50 +70,53 @@ def ceildiv( needed, per_run ):
 class Factory( object ):
     """
     >>> import collections
-    >>> collections.Counter( Factory( EXAMPLE_NANOFACTORY ).bill_of_materials( { 'FUEL': -1 }, ['ORE'] ) )
+    >>> collections.Counter( Factory( EXAMPLE_NANOFACTORY ).require(1,'FUEL').bill_of_materials( ['ORE'] ) )
     Counter({'ORE': -31})
     """
     def __init__( self, program ):
         self._productions = prepare_factory( parse_productions( program ) )
-    
-    def bill_of_materials( self, inventory, terminals):
-        """ Inventory is a defaultdict(int), representing the number of doses of each chemical
-            available.  A negative amount represents the need to produce a chemical, unless its
-            name appears in terminals.
-        >>> Factory( "1 A => 1 B" ).bill_of_materials( {'B': -1}, ['A'] )
+        self._inventory={}
+  
+    def require(self, quantity, chemical ):
+        self._inventory[chemical]=self._inventory.get(chemical,0)-quantity
+        return self
+ 
+    def first_deficit( self, terminals ):
+        for chemical, quantity in self._inventory.items():
+            if chemical in terminals:
+                continue
+            if quantity >= 0:
+                continue
+            return chemical, quantity
+        return None, 0
+
+    def bill_of_materials( self, terminals):
+        """
+        >>> Factory( "1 A => 1 B" ).require( 1, 'B' ).bill_of_materials( ['A'] )
         {'A': -1}
-        >>> Factory( "1 A => 1 B" ).bill_of_materials( {'B': -2}, ['A'] )
+        >>> Factory( "1 A => 1 B" ).require( 2, 'B' ).bill_of_materials( ['A'] )
         {'A': -2}
-        >>> Factory( "1 A, 2 B => 1 C" ).bill_of_materials( {'C': -2}, ['A', 'B'] )
+        >>> Factory( "1 A, 2 B => 1 C" ).require( 2, 'C').bill_of_materials( ['A', 'B'] )
         {'A': -2, 'B': -4}
-        >>> Factory( "1 A, 2 B => 2 C" ).bill_of_materials( {'C': -4}, ['A', 'B'] )
+        >>> Factory( "1 A, 2 B => 2 C" ).require( 4, 'C').bill_of_materials( ['A', 'B'] )
         {'A': -2, 'B': -4}
-        >>> Factory( "1 A, 2 B => 3 C" ).bill_of_materials( {'C': -4}, ['A', 'B'] )
+        >>> Factory( "1 A, 2 B => 3 C" ).require( 4, 'C').bill_of_materials( ['A', 'B'] )
         {'A': -2, 'B': -4}
-        >>> Factory( "1 A, 2 B => 4 C" ).bill_of_materials( {'C': -4}, ['A', 'B'] )
+        >>> Factory( "1 A, 2 B => 4 C" ).require( 4, 'C').bill_of_materials( ['A', 'B'] )
         {'A': -1, 'B': -2}
-        >>> Factory( "1 A, 2 B => 4 C\\n 4 A => 1 B" ).bill_of_materials( {'C': -4}, ['A', 'B'] )
+        >>> Factory( "1 A, 2 B => 4 C\\n 4 A => 1 B" ).require( 4, 'C').bill_of_materials( ['A', 'B'] )
         {'A': -1, 'B': -2}
-        >>> Factory( "1 A, 2 B => 4 C\\n 4 A => 1 B" ).bill_of_materials( {'C': -4}, ['A'] )
+        >>> Factory( "1 A, 2 B => 4 C\\n 4 A => 1 B" ).require( 4, 'C').bill_of_materials( ['A'] )
         {'A': -9}
         """
-        def first_deficit( inventory, terminals ):
-            for chemical, quantity in inventory.items():
-                if chemical in terminals:
-                    continue
-                if quantity >= 0:
-                    continue
-                return chemical, quantity
-            return None, 0
  
-        chemical, quantity = first_deficit( inventory, terminals )
+        chemical, quantity = self.first_deficit( terminals )
         while quantity<0:
-            have_doubts=True
             run_yield, recipe = self._productions[chemical]
             runs=ceildiv( -quantity, run_yield )
             for q, c in recipe:
-                inventory[c]=inventory.get(c,0)-q
-            inventory[chemical]=inventory.get(chemical,0)+run_yield
-            chemical, quantity = first_deficit( inventory, terminals )
+                self._inventory[c]=self._inventory.get(c,0)-q
+            self._inventory[chemical]=self._inventory.get(chemical,0)+run_yield
+            chemical, quantity = self.first_deficit( terminals )
 
-        return { k: v for k, v in inventory.items() if v < 0 }
+        return { k: v for k, v in self._inventory.items() if v < 0 }
