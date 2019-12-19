77d6c068cab07b5f0aabbbf9084c5a543e87155f
Steve Carter <sweavo@gmail.com>
2019-12-19 13:57:27 +0000

    refactor. did it go faster? 10.614 seconds; fft 3.166 over 100 calls

--- day16.py
+++ day16.py
@@ -2,6 +2,28 @@
 
 from signal_day16 import DAY16_SIGNAL
 
+def prep( signal ):
+    """ turn the input, which might be a large int or string, into a list of ints, one for each digit 
+    >>> prep([1,2,5,6])
+    [1, 2, 5, 6]
+    >>> prep(8934)
+    [8, 9, 3, 4]
+    >>> prep('4590')
+    [4, 5, 9, 0]
+    """
+    if isinstance(signal, int):
+        signal=str(signal)
+    if isinstance(signal, str):
+        signal=list(map(int,signal))
+    return signal
+
+def present( signal_list ):
+    """
+    >>> present([1,3,2,4,3,5,4,6])
+    '13243546'
+    """
+    return ''.join( map( str, signal_list ) )
+
 def pattern_for_phase( p, size ):
     """ Generator function for the repeating pattern for Flawed Frequency Transmission algorithm
     >>> list(pattern_for_phase( 0, 5 ))
@@ -13,59 +35,40 @@ def pattern_for_phase( p, size ):
     >>> list(pattern_for_phase( 2, 15 ))
     [0, 0, 1, 1, 1, 0, 0, 0, -1, -1, -1, 0, 0, 0, 1]
     """
-    for c in range(1,size+1):
-        yield [0,1,0,-1][(c//(p+1))%4]
-
-def last_digit( ii ):
-    """
-    >>> last_digit( 12 )
-    '2'
-    >>> last_digit( -17 )
-    '7'
-    >>> last_digit( 0 )
-    '0'
-    """
-    return str(ii)[-1]
-
-def digits_of( signal ):
-    """ each digit, treated numerically
-    >>> list(digits_of( 123456 ))
-    [1, 2, 3, 4, 5, 6]
-    >>> list(digits_of( '051627' ))
-    [0, 5, 1, 6, 2, 7]
-    """
-    return map(int,str(signal))
+    return [[0,1,0,-1][(c//(p+1))%4] for c in range(1,size+1)]
 
 def fft( signal ):
     """
-    >>> fft( 12345678 )
+    >>> present(fft( [1,2,3,4,5,6,7,8] ))
     '48226158'
-    >>> fft( '48226158' )
+    >>> present(fft( [4,8,2,2,6,1,5,8] ))
     '34040438'
-    >>> fft( '34040438' )
+    >>> present(fft( [3,4,0,4,0,4,3,8] ))
     '03415518'
     """ 
-    response = ''
-    length=len(str(signal))
+    response = []
+    length=len(signal)
     for p in range(length):
         total=0
-        for n, m in zip( digits_of(signal), pattern_for_phase( p, length ) ):
+        for n, m in zip( signal, pattern_for_phase( p, length ) ):
             if m>0:
                 total+=n
             elif m<0:
                 total-=n
-        response+=(last_digit( total ) )
+        response.append( abs(total) % 10 )
     return response
 
 def fftXn( signal,n=100 ):
     """
-    >>> fftXn( 80871224585914546619083218645595 )
+    >>> present(fftXn( prep(80871224585914546619083218645595) ))
     '24176176'
-    >>> fftXn( '19617804207202209144916044189917' )
+    >>> present(fftXn( prep('19617804207202209144916044189917') ))
     '73745418'
-    >>> fftXn( '69317163492948606335995924319873' )
+    >>> present(fftXn( prep('69317163492948606335995924319873') ))
     '52432133'
     """
+    if isinstance(signal, str):
+        signal = list(map(int,signal))
     for x in range(n):
         signal=fft(signal)
     return signal[:8]
@@ -75,14 +78,14 @@ def day16part1():
     >>> day16part1()
     '11833188'
     """
-    return fftXn( DAY16_SIGNAL )
+    return present(fftXn( prep( DAY16_SIGNAL )))
 
 if __name__ == "__main__":
     #print (day16part1())
     import cProfile, pstats
     pr=cProfile.Profile()
     pr.enable()
-    fft( DAY16_SIGNAL )
+    fftXn( DAY16_SIGNAL )
     pr.disable()
     pr.print_stats()
      
